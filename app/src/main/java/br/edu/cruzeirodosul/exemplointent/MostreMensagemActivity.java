47f95e1bb1699e74b3bb4520576d5af7f23d48d6
Cassiano-Gunji <cassiano.gunji@gmail.com>
2022-10-05 04:43:24 -0300

    Intents externos implementados

--- app/src/main/java/br/edu/cruzeirodosul/exemplointent/MostreMensagemActivity.java
+++ app/src/main/java/br/edu/cruzeirodosul/exemplointent/MostreMensagemActivity.java
@@ -2,11 +2,17 @@ package br.edu.cruzeirodosul.exemplointent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MostreMensagemActivity extends AppCompatActivity {
+
+    private String mensagem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,11 +21,37 @@ public class MostreMensagemActivity extends AppCompatActivity {
 
         // Obtém a intent que iniciou esta activity e extrai o string
         Intent intent = getIntent();
-        String mensagem = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+        mensagem = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
 
         // Captura o TextView do layout e define o string como seu Text.
         TextView textView = findViewById(R.id.textView);
         textView.setText(mensagem);
+    }
+
+    public void procureNaInternet(View view){
+        Uri pesquisa = Uri.parse("https://www.google.com/search?q=" + mensagem);
+        Intent intentPesquisa = new Intent(Intent.ACTION_VIEW, pesquisa);
+        try {
+            startActivity(intentPesquisa);
+        } catch(ActivityNotFoundException e){
+            String erro = getResources().getString(R.string.erroIntent);
+            Toast toast = Toast.makeText(this, erro, Toast.LENGTH_SHORT);
+            toast.show();
+        }
+    }
+
+    public void procureUmLocal(View view){
+        Uri local = Uri.parse("geo:0,0?q=" + mensagem);
+        Intent intentLocal = new Intent(Intent.ACTION_VIEW, local);
+        String titulo = getResources().getString(R.string.escolhaApp);
+        Intent intentChooser = Intent.createChooser(intentLocal, titulo);
+        try {
+            startActivity(intentChooser);
+        } catch(ActivityNotFoundException e){
+            String erro = getResources().getString(R.string.erroIntent);
+            Toast toast = Toast.makeText(this, erro, Toast.LENGTH_SHORT);
+            toast.show();
+        }
     }
 }
 
